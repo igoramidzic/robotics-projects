2c7ea3b30262c97ec712ce3bd2224480ecaa5586
Igor Amidzic <amidzicigor46@gmail.co>
2020-01-22 14:09:06 -0500

    Remove icon from sample programs

--- sample_programs/python/4_ServoSetup/servos.py
+++ sample_programs/python/4_ServoSetup/servos.py
@@ -15,18 +15,21 @@ RSERVO = 1
 
 # This function is called when Ctrl+C is pressed.
 # It's intended for properly exiting the program.
+
+
 def ctrlC(signum, frame):
     print("Exiting")
-    
+
     # Stop the servos
-    pwm.set_pwm(LSERVO, 0, 0);
-    pwm.set_pwm(RSERVO, 0, 0);
-    
+    pwm.set_pwm(LSERVO, 0, 0)
+    pwm.set_pwm(RSERVO, 0, 0)
+
     exit()
+
 
 # Attach the Ctrl+C signal interrupt
 signal.signal(signal.SIGINT, ctrlC)
-    
+
 # Initialize the servo hat library.
 pwm = Adafruit_PCA9685.PCA9685()
 
@@ -34,22 +37,22 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
 
 # Write an initial value of 1.5, which keeps the servos stopped.
-# Due to how servos work, and the design of the Adafruit library, 
+# Due to how servos work, and the design of the Adafruit library,
 # the value must be divided by 20 and multiplied by 4096.
-pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096));
-pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096));
+pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096))
+pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096))
 
 while True:
     # Write a maximum value of 1.7 for each servo.
     # Since the servos are oriented in opposite directions,
     # the robot will end up spinning in one direction.
     # Values between 1.3 and 1.7 should be used.
-    pwm.set_pwm(LSERVO, 0, math.floor(1.6 / 20 * 4096));
-    pwm.set_pwm(RSERVO, 0, math.floor(1.6 / 20 * 4096));
+    pwm.set_pwm(LSERVO, 0, math.floor(1.6 / 20 * 4096))
+    pwm.set_pwm(RSERVO, 0, math.floor(1.6 / 20 * 4096))
     time.sleep(4)
-    
+
     # Write a minimum value of 1.4 for each servo.
     # The robot will end up spinning in the other direction.
-    pwm.set_pwm(LSERVO, 0, math.floor(1.4 / 20 * 4096));
-    pwm.set_pwm(RSERVO, 0, math.floor(1.4 / 20 * 4096));
+    pwm.set_pwm(LSERVO, 0, math.floor(1.4 / 20 * 4096))
+    pwm.set_pwm(RSERVO, 0, math.floor(1.4 / 20 * 4096))
     time.sleep(4)
